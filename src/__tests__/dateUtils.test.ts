d7a84b9a6a24ba933c94a277ca28738f961c8c8f
DanielB1411 <danielb21@ru.is>
2024-02-11 18:33:11 +0000

    added Type definition to arguments and functions for dateUtils.ts

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -1,5 +1,15 @@
-import {describe} from 'vitest';
+import {describe,assert,expect,it} from 'vitest';
+import { getCurrentYear } from '../dateUtils';
+
 
 describe("Date Utils", () => {
   // Add your tests here
+  it("should run getCurrentYear", () => {
+
+    const currentYear = getCurrentYear()
+    const compareCurrentYear:number = 2024
+    expect(currentYear).toEqual(compareCurrentYear)
+  
+  })
+
 });
